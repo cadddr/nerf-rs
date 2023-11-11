63d49c696abe89a077594a89503c999831ecf873
(car (cdr (cdr (cdr _))) <cadddr@users.noreply.github.com>
2023-11-10 22:04:27 -0800

    light ref

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@
 //  and outputs σ and a 256-dimensional feature vector. This feature vector is then concatenated with the camera ray’s viewing direction and passed to one additional
 //  fully-connected layer (using a ReLU activation and 128 channels) that output the view-dependent RGB color.
 //and using volume ren- dering techniques to composite these values into an image (c
-//TODO: curious how it would do without relying on volume rendering approximation and density
 mod ray_sampling;
 use ray_sampling::{HEIGHT, WIDTH};
 
@@ -15,7 +14,7 @@ mod image_loading;
 mod input_transforms;
 
 mod model_dfdx;
-use model_dfdx::{prediction_as_u32, prediction_array_as_u32, from_u8_rgb, rgba_to_u8_array};
+use model_dfdx::{from_u8_rgb, prediction_array_as_u32, prediction_as_u32, rgba_to_u8_array};
 
 mod model_tch;
 
@@ -26,110 +25,137 @@ use textplots::{Chart, Plot, Shape};
 
 use clap::Parser;
 
-use std::fs::File;
-use std::io::{Write, BufReader, BufRead, Error};
-
+use std::time::SystemTime;
 use tensorboard_rs::summary_writer::SummaryWriter;
 
 const DEBUG: bool = false;
-const REFRESH_EPOCHS: usize = 100;
-const num_iter: usize = 50000;
-const eval_steps: usize = 200;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, default_value = "spheres/image-0.png")]
+    img_path: String,
+
+    #[arg(long, default_value = "logs")]
+    log_dir: String,
+
+    #[arg(long, default_value = "checkpoints")]
+    save_dir: String,
+
+    #[arg(long, default_value = "")]
+    load_path: String,
+
+    #[arg(long, default_value_t = 50000)]
+    num_iter: usize,
+
+    #[arg(long, default_value_t = 500)]
+    eval_steps: usize,
+
+    #[arg(long, default_value_t = 100)]
+    refresh_epochs: usize,
+}
+
 // TODO:
 /*
 - sample highest error samples
 - predict shading as different channels
 */
 
-#[derive(Parser)]
-struct Cli {
-	#[arg(default_value = "spheres/image-0.png")]
-    img_path: String,
-	
-	#[arg(default_value = "./logdir")]
-    log_dir: String,
-	
-	#[arg(default_value = "checkpoint.ot")]
-	save_path: String,
-	
-	#[arg(default_value = "")]
-	load_path: String
-}
-
 fn main() {
-	/*
-	Main loop. Reads image(s), inits model, runs train loop (within window refresh handler);
-	on eval - draw to backbuffer, which is displayed on every frame
-	*/
-	
-	let args = Cli::parse();
-	let mut writer = SummaryWriter::new(&args.log_dir);
-	
+    /*
+    Main loop. Reads image(s), inits model, runs train loop (within window refresh handler);
+    on eval - draw to backbuffer, which is displayed on every frame
+    */
+    let ts = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let args = Cli::parse();
+
     let img = image_loading::load_image_as_array(&args.img_path);
     println!("image {:?} pixels", img.len());
+
     let mut backbuffer = [0; WIDTH * HEIGHT];
 
-	// let mut model = model_dfdx::DfdxMlp::new();
-	let mut model = model_tch::TchModel::new();
-	if args.load_path != "" {
-		model.load(&args.load_path);
-	}
+    // let mut model = model_dfdx::DfdxMlp::new();
+    let mut model = model_tch::TchModel::new();
+    if args.load_path != "" {
+        model.load(&args.load_path);
+    }
 
-	let mut iter = 0;
-	let mut batch_losses: Vec<f32> = Vec::new();
-    let mut update_window_buffer = |buffer: &mut Vec<u32>| {
-        if !DEBUG {
-			//predict emittance and density
-			let (indices, views, points) = ray_sampling::sample_points_tensor_along_view_directions(model.BATCH_SIZE());
-			let gold: Vec<[f32; 4]> = indices.iter().map(|[y, x]| img[y * WIDTH + x]).collect();
-			let screen_coords: Vec<[f32; model_tch::INDIM]> = indices.iter().map(input_transforms::scale_by_screen_size_and_fourier::<3>).collect();
-			
-			let predictions = model.predict(points.clone(), views.clone(), screen_coords.clone());
-			
-			if (iter % eval_steps == 0) {
-				// refresh backbuffer every few steps
-	            if (batch_losses.len() * model.BATCH_SIZE()) % (img.len() * REFRESH_EPOCHS) == 0 {
-	                backbuffer = [0; WIDTH * HEIGHT];
-	            }
-				
-				// write batch predictions to backbuffer to display until next eval
-				for ([y, x], prediction) in indices[..model.BATCH_SIZE()].iter().zip(model.get_predictions_as_array_vec(&predictions).into_iter()) {
-                	backbuffer[y * WIDTH + x] = prediction_array_as_u32(&prediction);
-            	}
-				
-				writer.add_image("prediction", &backbuffer.iter().map(rgba_to_u8_array).flatten().collect::<Vec<u8>>(), &vec![3, WIDTH, HEIGHT][..], iter);
-				model.save(&args.save_path);
-			}
+    let mut iter = 0;
+    let mut writer = SummaryWriter::new(&args.log_dir);
 
-			let loss: f32 = model.step(predictions, gold[..model.BATCH_SIZE()].to_vec());
-			// loss & plotting
-            println!("iter={}, loss={:.16}", iter, loss);
-			writer.add_scalar("loss", loss, iter);
-			
-			batch_losses.push(loss);
-            Chart::new(120, 40, 0., batch_losses.len() as f32)
-            	.lineplot(&Shape::Continuous(Box::new(|x| batch_losses[x as usize])))
-            	.display();
+    let mut batch_losses: Vec<f32> = Vec::new();
+    let update_window_buffer = |buffer: &mut Vec<u32>| {
+        //predict emittance and density
+        let (indices, views, points) =
+            ray_sampling::sample_points_tensor_along_view_directions(model.BATCH_SIZE());
+        let gold: Vec<[f32; 4]> = indices.iter().map(|[y, x]| img[y * WIDTH + x]).collect();
+        let screen_coords: Vec<[f32; model_tch::INDIM]> = indices
+            .iter()
+            .map(input_transforms::scale_by_screen_size_and_fourier::<3>)
+            .collect();
+
+        let predictions = model.predict(points.clone(), views.clone(), screen_coords.clone());
+
+        if iter % args.eval_steps == 0 {
+            // refresh backbuffer every few steps
+            if (batch_losses.len() * model.BATCH_SIZE()) % (img.len() * args.refresh_epochs) == 0 {
+                backbuffer = [0; WIDTH * HEIGHT];
+            }
+
+            // write batch predictions to backbuffer to display until next eval
+            for ([y, x], prediction) in indices
+                .iter()
+                .zip(model.get_predictions_as_array_vec(&predictions).into_iter())
+            {
+                backbuffer[y * WIDTH + x] = prediction_array_as_u32(&prediction);
+            }
+
+            writer.add_image(
+                "prediction",
+                &backbuffer
+                    .iter()
+                    .map(rgba_to_u8_array)
+                    .flatten()
+                    .collect::<Vec<u8>>(),
+                &vec![3, WIDTH, HEIGHT][..],
+                iter,
+            );
+            model.save(&format!("{}/checkpoint-{}-{}.ot", args.save_dir, ts, iter));
         }
-		
-		draw_to_screen(buffer, &backbuffer, &img);
-		iter = iter + 1;
-		if iter > num_iter {
-			panic!("Reached maximum iterations")
-		}
+
+        let loss: f32 = model.step(predictions, gold);
+        // loss & plotting
+        println!("iter={}, loss={:.16}", iter, loss);
+        writer.add_scalar("loss", loss, iter);
+
+        batch_losses.push(loss);
+        Chart::new(120, 40, 0., batch_losses.len() as f32)
+            .lineplot(&Shape::Continuous(Box::new(|x| batch_losses[x as usize])))
+            .display();
+
+        draw_to_screen(buffer, &backbuffer, &img);
+        iter = iter + 1;
+        if iter > args.num_iter {
+            panic!("Reached maximum iterations")
+        }
     };
     run_window(update_window_buffer, WIDTH, HEIGHT);
 }
 
 fn draw_to_screen(buffer: &mut Vec<u32>, backbuffer: &[u32; WIDTH * HEIGHT], img: &Vec<[f32; 4]>) {
-	// draw from either backbuffer or gold image
-   for y in 0..HEIGHT {
+    // draw from either backbuffer or gold image
+    for y in 0..HEIGHT {
         for x in 0..WIDTH {
             if DEBUG {
                 let gold = img[y * WIDTH + x];
-                buffer[y * WIDTH + x] = from_u8_rgb((gold[0] * 255.) as u8, (gold[1]*255.) as u8, (gold[2]*255.) as u8);
-            }
-            else{
+                buffer[y * WIDTH + x] = from_u8_rgb(
+                    (gold[0] * 255.) as u8,
+                    (gold[1] * 255.) as u8,
+                    (gold[2] * 255.) as u8,
+                );
+            } else {
                 buffer[y * WIDTH + x] = backbuffer[y * WIDTH + x];
             }
         }
